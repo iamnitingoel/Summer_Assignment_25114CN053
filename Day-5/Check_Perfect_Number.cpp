c4ab2555e8755831f9b744d94f46be95f9dbc041
Nitin <108nitin.goel@gmail.com>
2026-06-01 23:58:57 +0530

    Day-5 uploaded.

---
+++ Day-5/Check_Perfect_Number.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n,sum=0;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(long long i=1;i<n;i++){
+        if(n%i==0){
+            sum+=i;
+        }
+    }
+    if(n==sum){
+        cout<<n<<" is a Perfect Number.";
+    }
+    else{
+        cout<<n<<" is NOT a Perfect Number.";
+    }
+}
