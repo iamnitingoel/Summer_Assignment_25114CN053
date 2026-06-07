1ec49c398ff9fff1c632b85c789dff151ad2a85b
Nitin <108nitin.goel@gmail.com>
2026-06-07 17:36:50 +0530

    Fix prime number check for n<=1

--- Day-3/Check_prime.cpp
+++ Day-3/Check_prime.cpp
@@ -4,15 +4,20 @@ int main(){
     int n,flag=0;
     cout<<"Enter a number: ";
     cin>>n;
-    for(int i=2;i<n;i++){
-        if(n%i==0){
-            cout<<n<<" is not a prime number";
-            flag=1;
-            break;
-        }
+    if(n<=1){
+        cout<<n<<" is NOT a prime number.";
     }
-    if(flag==0){
-        cout<<n<<" is a prime number";
+    else{
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                cout<<n<<" is NOT a prime number";
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0){
+            cout<<n<<" is a prime number";
+        }
     }
     return 0;
 }
